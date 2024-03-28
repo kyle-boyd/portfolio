da462aeff210a11a48a6346de54e9285d291d8da
Kyle Boyd <105263259+kyle-boyd@users.noreply.github.com>
2024-03-28 12:33:34 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+// Function to handle the intersection of observed elements
+const handleIntersection = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show'); // Remove the 'show' class when not intersecting
+    }
+  });
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, {
+  threshold: 0.2 // Trigger when 20% of the element is visible
+});
+
+// Get all sections
+const sections = document.querySelectorAll('.animatesection');
+
+// Observe each section
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+
 $(document).ready(function() {
     $('#submitBtn').click(function(e) {
         e.preventDefault();
         var enteredPassword = $('#password').val();
-        var correctPassword = "your_password_here"; // Change this to your desired password
+        var correctPassword = "kyleboyd2024"; // Change this to your desired password
 
         if (enteredPassword === correctPassword) {
             window.location.href = "protected_page.html";
@@ -11,3 +36,4 @@ $(document).ready(function() {
         }
     });
 });
+
